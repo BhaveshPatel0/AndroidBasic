ebd7bea25d1457784f2cc1ac1b5eff40de43f0e9
Bhavesh <Bhavesh05081998@Gmail.com>
2024-01-05 22:46:32 +0530

    Complete Nested While Loop

--- Practice/app/src/main/java/com/example/practiceapplication/MainActivity.kt
+++ Practice/app/src/main/java/com/example/practiceapplication/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.practiceapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.practiceapplication.databinding.ActivityMainBinding
-import com.example.practiceapplication.result.ResultAct
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,10 +11,58 @@ class MainActivity : AppCompatActivity() {
     var output = ""
     var count = 0
     var whileCounter = 0
+
+    var alphaCounter=0
+
+
+    val listOfAlphabet= arrayListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        listOfAlphabet.add("A")
+        listOfAlphabet.add("B")
+        listOfAlphabet.add(" C")
+        listOfAlphabet.add("D")
+        listOfAlphabet.add(" E")
+        listOfAlphabet.add(" F")
+        listOfAlphabet.add("G")
+        listOfAlphabet.add(" H")
+        listOfAlphabet.add(" I")
+        listOfAlphabet.add(" J")
+        listOfAlphabet.add("K")
+        listOfAlphabet.add(" L")
+        listOfAlphabet.add(" M")
+        listOfAlphabet.add(" N")
+        listOfAlphabet.add(" O")
+
+
+//        listOfAlphabet.get(alphaCounter)
+
+        for (row in 0..listOfAlphabet.size){
+
+            for (column in 0..alphaCounter){
+                output=output.plus(listOfAlphabet.get(alphaCounter))
+                alphaCounter++
+            }
+
+            output=output.plus("\n")
+
+            Log.d("TAG1", "onCreate:$output ")
+
+        }
+
+        while (listOfAlphabet[alphaCounter]<alphaCounter.toString()){
+
+        }
+
+
+
+
+
+
 
         bindClick()
 
@@ -82,17 +128,13 @@ class MainActivity : AppCompatActivity() {
 
 
     fun patternWhileLoop(number: Int): String {
-
-
         while (number > count) {
-
-
             count++
-
             output=output.plus(count).plus("\n")
         }
         return output
     }
+
 
 
     //11111
@@ -137,4 +179,22 @@ class MainActivity : AppCompatActivity() {
 //        }
         return output
     }
+
+
+
+        object KboatStringPattern {
+            @JvmStatic
+            fun main(args: Array<String>) {
+                var ch = 'A'
+                for (i in 0..4) {
+                    for (j in 0..i) {
+                        print(ch++)
+                    }
+                    println()
+                }
+            }
+        }
+
+
+
 }
---
+++ Practice/app/src/main/java/com/example/practiceapplication/result/TaskList.kt
@@ -0,0 +1,28 @@
+package com.example.practiceapplication.result
+
+/**
+ * 1 Use list and create pattern
+ * alpha[a,b,c,d..]
+ * counter =0
+ * alpha[counter]
+ * counter++
+*/
+//A
+//B C
+//D E F
+//G H I J
+//K L M N O
+
+
+/**
+ * 2 Simple psiirtn
+* */
+//1
+//**
+//333
+//****
+//55555
+
+/**
+ * 3 Create pattern in recyclerview
+ * */
